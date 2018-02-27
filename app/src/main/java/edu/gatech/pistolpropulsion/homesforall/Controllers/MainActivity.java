203d099b794eb92225507fdf4592ff27ee49ac86
mikebwin <mikebwin@gmail.com>
2018-02-27 10:47:20 -0500

    trying to do recycler view

--- app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/MainActivity.java
+++ app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/MainActivity.java
@@ -3,15 +3,28 @@ package edu.gatech.pistolpropulsion.homesforall.Controllers;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.InputStreamReader;
+
+import edu.gatech.pistolpropulsion.homesforall.Models.DataReader;
+import edu.gatech.pistolpropulsion.homesforall.Models.ShelterManager;
 import edu.gatech.pistolpropulsion.homesforall.R;
 
 public class MainActivity extends Activity {
 
     private TextView logout;
+    private Button refresh;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,8 @@ public class MainActivity extends Activity {
         overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
 
         logout = (TextView) findViewById(R.id.logout_textView);
+        refresh = (Button) findViewById(R.id.refresh_button);
+        recyclerView = (RecyclerView) findViewById(R.id.shelterList);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,10 +43,86 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this, Welcome.class));
             }
         });
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                InputStreamReader csvfile = new InputStreamReader(getResources().openRawResource(R.raw.file));
+                DataReader reader = new DataReader(csvfile);
+                reader.read();
+
+//                System.out.println(reader.getCount());
+//                for(int i = 0; i < reader.getCount(); i++) {
+//                    for (int j = 0; j < 9; j++)
+//                        System.out.print(reader.getContent()[i][j]);
+//                    System.out.println();
+//                }
+
+                ShelterManager shelterManager = new ShelterManager(reader.getContent(), reader.getCount());
+//                Shelter[] shelterArray = shelterManager.getShelterArray();
+//                String[] namesArray = shelterManager.getNamesArray();
+//                for(int i = 0; i < 13; i++) {
+//                    System.out.println(namesArray[i]);
+//                }
+                mLayoutManager = new LinearLayoutManager(getApplicationContext());
+                recyclerView.setLayoutManager(mLayoutManager);
+
+
+                ArrayAdapter<String> namesAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                        android.R.layout.simple_list_item_1,
+                        shelterManager.getNamesArray());
+                recyclerView.setAdapter(namesAdapter);
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
         //do nothing
+    }
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public ViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.my_text_view, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.mTextView.setText(mDataset[position]);
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
     }
 }
