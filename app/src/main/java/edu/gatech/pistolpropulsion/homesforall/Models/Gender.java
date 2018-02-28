72dd6ef194c9fab8232f09340fa77164a75f0a3f
vab10266 <vaudalexburton@gmail.com>
2018-02-28 09:45:52 -0500

    user update
    added instance variables and gender enum{MALE, FEMALE, OTHER}

---
+++ app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Models/Gender.java
@@ -0,0 +1,5 @@
+package edu.gatech.pistolpropulsion.homesforall.Models;
+
+public enum Gender {
+    Male, Female, Other
+}
--- app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Models/User.java
+++ app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Models/User.java
@@ -1,27 +1,66 @@
 package edu.gatech.pistolpropulsion.homesforall.Models;
 
+/**
+ * Created by Yuan Zhang on 2/11/2018.
+ */
+
 public class User {
     private String email; //also username
-    private String name;
     private String pwd;
     private boolean locked;
+    private String name;
     private boolean isVeteran;
+    private Gender gender;
 
     public User(String email, String pwd) {
-        this(email, pwd, "", false);
+        this(email, pwd, "", false, null);
     }
-
-    public User(String email, String pwd, String name, boolean isVeteran) {
+    public User(String email, String pass, String name, boolean isVet, Gender gen){
         this.email = email;
-        this.pwd = pwd;
-        this.locked = false; //always default to unlocked
-        this.isVeteran = isVeteran;
+        this.pwd = pass;
+        this.locked = false;
         this.name = name;
+        this.isVeteran = isVet;
+        this.gender = gen;
     }
 
-    public String getName() { return name; }
-    public String getEmail() { return email; }
-    public Boolean getLocked() { return locked; }
-    public Boolean getVeteran() { return isVeteran; }
-
+    public User() {
+        this("", "");
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPass() {
+        return this.pwd;
+    }
+    public void setPass(String pwd) {
+        this.pwd = pwd;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String email) {
+        this.name = email;
+    }
+    public boolean isLocked() {
+        return this.locked;
+    }
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+    public boolean getVeteran() {
+        return this.isVeteran;
+    }
+    public void setVeteran(boolean vet) {
+        this.isVeteran = vet;
+    }
+    public Gender getGender() {
+        return this.gender;
+    }
+    public void setGender(Gender gen) {
+        this.gender = gen;
+    }
 }
