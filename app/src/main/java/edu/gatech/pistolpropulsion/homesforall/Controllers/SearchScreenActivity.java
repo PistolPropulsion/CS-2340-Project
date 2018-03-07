9159659d4a284e6efd4a2ef693e321eea217aa4a
CallmeJoeBob <joel.joseprabu@gmail.com>
2018-03-07 11:54:58 -0500

    Created SearchScreenActivity
    Created an activity for the search screen and linked it to the layout file

---
+++ app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/SearchScreenActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.pistolpropulsion.homesforall.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.pistolpropulsion.homesforall.R;
+
+public class SearchScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchscreen);
+    }
+}
