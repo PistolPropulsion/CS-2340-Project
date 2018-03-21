353eb9e529ddc36a1f51ae59db850e03a46f28d9
Amaan Marfatia <amaanmarfatia@gmail.com>
2018-03-21 16:59:43 -0400

    Add shelter calling functionality
    Allows user to call the shelter directly from the app through the ShelterDetailsActivity

--- app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/ShelterDetailsActivity.java
+++ app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/ShelterDetailsActivity.java
@@ -1,15 +1,24 @@
 package edu.gatech.pistolpropulsion.homesforall.Controllers;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
 
 import edu.gatech.pistolpropulsion.homesforall.Models.Shelter;
 import edu.gatech.pistolpropulsion.homesforall.R;
 
 public class ShelterDetailsActivity extends Activity {
+
+    private final int REQUEST_CALL = 1;
+    private Intent callShelterIntent;
 
     private Shelter currentShelter;
     private TextView nameDisplay;
@@ -38,5 +47,20 @@ public class ShelterDetailsActivity extends Activity {
         notesDisplay.setText("Special Notes: " + currentShelter.getSpecialNotes());
         restrictDisplay.setText("Restrictions: " + currentShelter.getRestrictions());
 
+
+        phoneDisplay.setOnClickListener((v) -> {
+            callShelterIntent = new Intent(Intent.ACTION_CALL);
+            callShelterIntent.setData(Uri.parse("tel:" + currentShelter.getPhone()));
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                startActivity(callShelterIntent);
+            } else {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+                    Toast.makeText(this, "Phone calling permission needed to call the shelter.", Toast.LENGTH_SHORT).show();
+                }
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+            }
+        });
     }
 }
