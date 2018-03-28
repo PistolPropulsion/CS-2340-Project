aaca8e3bdad37bcae1abff5c7ead60e059e19dab
mikebwin <mikebwin@gmail.com>
2018-03-28 10:49:06 -0400

    fixed empty or null string error on login.

--- app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/LoginActivity.java
+++ app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/LoginActivity.java
@@ -2,8 +2,11 @@ package edu.gatech.pistolpropulsion.homesforall.Controllers;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -11,6 +14,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -29,6 +34,7 @@ public class LoginActivity extends Activity {
     private TextView register;
     private FirebaseAuth mAuth;
 
+    @RequiresApi(api = Build.VERSION_CODES.ECLAIR)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,9 +54,31 @@ public class LoginActivity extends Activity {
             public void onClick(View v) {
                 editUser = (EditText) findViewById(R.id.editText_login_username);
                 editPass = (EditText) findViewById(R.id.editText_login_password);
+                if (editUser.getText() == null || editUser.getText().length() <= 0
+                        || editPass.getText() == null || editPass.getText().length() <= 0) {
+                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 //just for testing lol
                 //startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 mAuth.signInWithEmailAndPassword(editUser.getText().toString(), editPass.getText().toString())
+//                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                            @Override
+//                            public void onSuccess(AuthResult authResult) {
+//                                Log.d(TAG, "signInWithEmail:success");
+//                                FirebaseUser user = mAuth.getCurrentUser();
+//                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.w(TAG, "signInWithEmail:failure", e);
+//                                Toast.makeText(getApplicationContext(), "Authentication failed.",
+//                                        Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
                         .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
