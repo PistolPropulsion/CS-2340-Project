20a108c68aad3c29a0fae9be814e5c4868e23a36
mikebwin <mikebwin@gmail.com>
2018-04-23 18:34:46 -0400

    locks out log in after 3 unsuccessful attempts. to reset, at the moment, must go into firebase and manually reset the attempts field

--- app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/LoginActivity.java
+++ app/src/main/java/edu/gatech/pistolpropulsion/homesforall/Controllers/LoginActivity.java
@@ -17,7 +17,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.gatech.pistolpropulsion.homesforall.Models.User;
 import edu.gatech.pistolpropulsion.homesforall.R;
 
 import static android.content.ContentValues.TAG;
@@ -42,63 +52,72 @@ public class LoginActivity extends Activity {
         TextView cancel = findViewById(R.id.textView_login_cancelButton);
         TextView register = findViewById(R.id.textView_welcome_registerButton);
         TextView resetPass = findViewById(R.id.textView_login_resetPassButton);
+
+
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-
+        FirebaseDatabase mRef = FirebaseDatabase.getInstance();
+        final User[] user1 = new User[1];
 
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editUser = findViewById(R.id.editText_login_email);
                 editPass = findViewById(R.id.editText_login_password);
-                if ((editUser.getText() == null) || (editUser.getText().length() <= 0)
-                        || (editPass.getText() == null) || (editPass.getText().length() <= 0)) {
-                    Toast.makeText(getApplicationContext(), "Authentication failed.",
-                            Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                //just for testing lol
-                //startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                mAuth.signInWithEmailAndPassword(editUser.getText().
-                        toString(), editPass.getText().toString())
-//                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-//                            @Override
-//                            public void onSuccess(AuthResult authResult) {
-//                                Log.d(TAG, "signInWithEmail:success");
-//                                FirebaseUser user = mAuth.getCurrentUser();
-//                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-//                            }
-//                        })
-//                        .addOnFailureListener(new OnFailureListener() {
-//                            @Override
-//                            public void onFailure(@NonNull Exception e) {
-//                                Log.w(TAG, "signInWithEmail:failure", e);
-//                                Toast.makeText(getApplicationContext(), "Authentication failed.",
-//                                        Toast.LENGTH_SHORT).show();
-//                            }
-//                        })
-                        .addOnCompleteListener(LoginActivity.this,
-                                new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success
-                                    // update UI with the signed-in user's information
-                                    Log.d(TAG, "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    startActivity(new Intent(LoginActivity.this,
-                                            MainActivity.class));
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG,
-                                            "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(getApplicationContext(),
-                                          "Authentication failed.", Toast.LENGTH_SHORT).show();
-                                }
 
-                                // ...
-                            }
-                        });
+                DatabaseReference usersReference = mRef.getReference().child("users").child("standardUsers");
+                usersReference.orderByChild("email").equalTo(editUser.getText().toString()).addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        Map<String, Object> userUpdates = new HashMap<>();
+                        user1[0] = dataSnapshot.getValue(User.class);
+
+                        if ((editUser.getText() == null) || (editUser.getText().length() <= 0)
+                                || (editPass.getText() == null) || (editPass.getText().length() <= 0)) {
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        mAuth.signInWithEmailAndPassword(editUser.getText().
+                                toString(), editPass.getText().toString())
+                                .addOnCompleteListener(LoginActivity.this,
+                                        new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                if (user1[0] != null && user1[0].getAttempts() <= 0) {
+                                                    Log.d(TAG, "signInWithEmail:locked out");
+                                                    Toast.makeText(getApplicationContext(),
+                                                            "Account locked. Please contact Admin", Toast.LENGTH_SHORT).show();
+                                                }else if (task.isSuccessful()) {
+                                                    // Sign in success
+                                                    // update UI with the signed-in user's information
+                                                    Log.d(TAG, "signInWithEmail:success");
+                                                    FirebaseUser user = mAuth.getCurrentUser();
+                                                    startActivity(new Intent(LoginActivity.this,
+                                                            MainActivity.class));
+                                                } else {
+                                                    // If sign in fails, display a message to the user.
+                                                    Log.w(TAG,
+                                                            "signInWithEmail:failure", task.getException());
+                                                    Toast.makeText(getApplicationContext(),
+                                                            "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                                    user1[0].failedAttempt();
+                                                    System.out.println(user1[0].getEmail() + " " + user1[0].getAttempts());
+                                                    userUpdates.put(dataSnapshot.getKey(), user1[0]);
+                                                    usersReference.updateChildren(userUpdates);
+                                                }
+                                            }
+                                        });
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {}
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                });
             }
         });
 
